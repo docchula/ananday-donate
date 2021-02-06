d4f58b7521f2b792072365783d045a690b6a2eae
Sarun Intaralawan <sarunint@docchula.com>
2021-02-07 06:32:26 +0700

    chore(deps): update angular and angular cli packages

--- src/app/recovery/recovery.component.spec.ts
+++ src/app/recovery/recovery.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { RecoveryComponent } from './recovery.component';
 
@@ -6,7 +6,7 @@ describe('RecoveryComponent', () => {
   let component: RecoveryComponent;
   let fixture: ComponentFixture<RecoveryComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [RecoveryComponent]
     }).compileComponents();
